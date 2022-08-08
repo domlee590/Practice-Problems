f7580ed498a9499d27647a1183e6bb3bed825ae5
Dominic Lee <domlee590@gmail.com>
2022-08-07 20:17:47 -0400

    Time: 3876 ms (59.00%), Space: 14.4 MB (13.84%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -0,0 +1,16 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        
+        # Create tabulation array (LIS starting at index)
+        # Seed base case, last index must be of length 1 (all of them actually)
+        T = [1 for _ in range(len(nums))]
+        
+        # Start iterating from second to last starting index
+        for i in range(len(nums) - 1, -1, -1):
+            # Check if we can combine the subsequences (latter starts larger)
+            for j in range(i + 1, len(nums)):
+                if nums[i] < nums[j]:
+                    T[i] = max(T[i], 1 + T[j])
+        
+        # Get the largest subsequence starting at some index
+        return max(T)
