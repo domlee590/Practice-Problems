8113707aa81c6e08d97859e374cf29b1df59d745
Dominic Lee <domlee590@gmail.com>
2022-10-23 20:21:25 -0400

    Time: 2187 ms (34.46%), Space: 13.8 MB (95.95%) - LeetHub

--- 1954-minimum-garden-perimeter-to-collect-enough-apples/1954-minimum-garden-perimeter-to-collect-enough-apples.py
+++ 1954-minimum-garden-perimeter-to-collect-enough-apples/1954-minimum-garden-perimeter-to-collect-enough-apples.py
@@ -1,15 +1,33 @@
 class Solution:
     def minimumPerimeter(self, neededApples: int) -> int:
-        lo, hi = 1, 100000
-        
-        while lo < hi:
-            m = (lo+hi)//2
-            n = 2*m*(m+1)*(2*m+1)
-            
-            if n < neededApples: 
-                lo = m+1
-            else: 
-                hi = m
+        '''
+        m = 1
+        n = 4 * (1+2)
 
-        return 8*lo
+        m = 2
+        n = 4 * (3+2+3+4)
+
+        m = 3
+        n = 4 * (5+4+3+4+5+6) 
+
+        m = m
+        n = 4 * (m + 2(m+1) + 2(m+2) + 2(m+3) + ... + 2(m+m-1) + m+m)
+          = 4 * [(m + 2*m + 2*m + 2*m + ... (m times)) +  2 * (1+2+3+...+(m-1))]
+          = 4 * [m + 2*m*m + 2 * m*(m-1)/2]
+          = 4 * [m + 2*m*m + m*m - m]
+          = 4 * [3*m*m]
+          = 12*m*m
+
+
+        N(m) = n(1) + n(2) + n(3) + ... + n(m)
+             = 12*1 + 12*4 + 12*9 + ... + 12*m*m
+             = 12 * (1 + 4 + 9 + ... + m*m)
+             = 12 * (m)*(m+1)*(2*m+1)/6
+             = 2 * m*(m+1)*(2*m+1)
+        '''
         
+        n = m = 0
+        while n < neededApples:
+            m += 1
+            n += 12*m*m
+        return 8*m
